2437b7c8fc0f61d5ef906a9a057f0697ed4b24a1
MarieBouafou <marie.bouafou@epfedu.fr>
2026-02-12 17:03:22 +0100

    Ajout de date dans notre page

--- essai1.py
+++ essai1.py
@@ -1,4 +1,16 @@
 import tkinter as tk
+from datetime import datetime
+
+
+def date():
+    now = datetime.now()
+    current_date = now.strftime("%d %B %Y")
+    date_label.config(text=current_date)
+    a.after(1000, date)  # run le programme apres 1second
+
 
 a = tk.Tk()  # fenetre de l'application
-a.mainloop()  # boucle principale de l'application
+date_label = tk.Label(a)
+date_label.pack(pady=5, fill="x")
+date()
+a.mainloop()  # run l'application
